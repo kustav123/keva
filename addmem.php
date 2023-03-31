e630d26cae6b2886f3af9e3357bc647e1fa49bf9
Bhargabiam <87924439+Bhargabiam@users.noreply.github.com>
2023-03-31 16:50:25 +0530

    addmem And nevbar
    nevbar new menu add and icon change, under profile section url edit .html to .php, page titel add in addmem page

--- addmem.php
+++ addmem.php
@@ -2,12 +2,12 @@
 <html lang="en">
 
 <head>
-<?php
+    <?php
 require_once("navbar.php");
 ?>
-  <title>Add Member</title>
+    <title>Add Member</title>
 
-  <!-- =======================================================
+    <!-- =======================================================
   * Template Name: NiceAdmin
   * Updated: Mar 09 2023 with Bootstrap v5.2.3
   * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
@@ -18,100 +18,123 @@ require_once("navbar.php");
 
 <body>
 
-  
+    <!-- <div id="layoutSidenav_content"> -->
+    <main id="main" class="main">
 
-  <main id="main" class="main">
-  <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-8">
-  <div class="card">
-  <div class="card-body">
-    <h5 class="card-title">Registration Form</h5>
+    <div class="pagetitle">
+      <h1>Add New Member</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item">User Manegment</li>
+          <li class="breadcrumb-item active">Add New Member</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
 
-    <!-- Registration Form -->
-    <form class="row g-3">
-    <div class="row mb-3">
-      <div class="col-md-6">
-        <div class="form-floating">
-          <input type="text" class="form-control" id="kevaID" placeholder="Keva ID" maxlength="10">
-          <label for="kevaID">Keva ID</label>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="form-floating">
-          <input type="text" class="form-control" id="fullName" placeholder="Full Name" maxlength="30">
-          <label for="fullName">Full Name</label>
-        </div>
-      </div>
-      </div>
-      <div class="row mb-3">
-      <div class="col-md-6">
-        <div class="form-floating">
-          <input type="text" class="form-control" id="username" placeholder="Username" maxlength="20">
-          <label for="username">Username</label>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="form-floating">
-          <input type="tel" class="form-control" id="mobileNumber" placeholder="Mobile Number" maxlength="10">
-          <label for="mobileNumber">Mobile Number</label>
-        </div>
-      </div>
-      </div>
+        <section class="section">
 
-      <div class="row mb-3"> 
-      <div class="col-md-6">
-        <div class="form-floating">
-          <select class="form-select" id="sponsorDropdown" aria-label="Sponsor">
-            <option selected disabled>Select Sponsor</option>
-            <option value="1">Sponsor 1</option>
-            <option value="2">Sponsor 2</option>
-            <option value="3">Sponsor 3</option>
-          </select>
-          <label for="sponsorDropdown">Sponsor</label>
-        </div>
-      </div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Registration Form</h5>
 
-      <div class="col-md-6">
-        <div class="form-floating">
-          <select class="form-select" id="placement" aria-label="Sponsor">
-            <option selected disabled>Hand Side</option>
-            <option value="1">Right</option>
-            <option value="2">Left</option>
-          </select>
-          <label for="placement">Placement</label>
-        </div>
-      </div>
-      </div>
+                                    <!-- Registration Form -->
+                                    <form class="row g-3">
+                                        <div class="row mb-3">
+                                            <div class="col-md-6">
+                                                <div class="form-floating">
+                                                    <input type="text" class="form-control" id="kevaID"
+                                                        placeholder="Keva ID" maxlength="10">
+                                                    <label for="kevaID">Keva ID</label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-floating">
+                                                    <input type="text" class="form-control" id="fullName"
+                                                        placeholder="Full Name" maxlength="30">
+                                                    <label for="fullName">Full Name</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <div class="col-md-6">
+                                                <div class="form-floating">
+                                                    <input type="text" class="form-control" id="username"
+                                                        placeholder="Username" maxlength="20">
+                                                    <label for="username">Username</label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-floating">
+                                                    <input type="tel" class="form-control" id="mobileNumber"
+                                                        placeholder="Mobile Number" maxlength="10">
+                                                    <label for="mobileNumber">Mobile Number</label>
+                                                </div>
+                                            </div>
+                                        </div>
 
-      <div class="row mb-3">
-      <div class="col-md-6">
-        <div class="form-floating">
-          <input type="password" class="form-control" id="password" placeholder="Password">
-          <label for="password">Password</label>
-        </div>
-      </div>
-      </div>
+                                        <div class="row mb-3">
+                                            <div class="col-md-6">
+                                                <div class="form-floating">
+                                                    <select class="form-select" id="sponsorDropdown"
+                                                        aria-label="Sponsor">
+                                                        <option selected disabled>Select Sponsor</option>
+                                                        <option value="1">Sponsor 1</option>
+                                                        <option value="2">Sponsor 2</option>
+                                                        <option value="3">Sponsor 3</option>
+                                                    </select>
+                                                    <label for="sponsorDropdown">Sponsor</label>
+                                                </div>
+                                            </div>
 
-      <div class="text-center">
-        <button type="submit" class="btn btn-primary">Submit</button>
-        <button type="reset" class="btn btn-secondary">Reset</button>
-      </div>
-    </form><!-- End Registration Form -->
+                                            <div class="col-md-6">
+                                                <div class="form-floating">
+                                                    <select class="form-select" id="placement" aria-label="Sponsor">
+                                                        <option selected disabled>Hand Side</option>
+                                                        <option value="1">Right</option>
+                                                        <option value="2">Left</option>
+                                                    </select>
+                                                    <label for="placement">Placement</label>
+                                                </div>
+                                            </div>
+                                        </div>
 
-  </div>
-</div>
-</div>
-</div>
-</div>
+                                        <div class="row mb-3">
+                                            <div class="col-md-6">
+                                                <div class="form-floating">
+                                                    <input type="password" class="form-control" id="password"
+                                                        placeholder="Password">
+                                                    <label for="password">Password</label>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                            <button type="reset" class="btn btn-secondary">Reset</button>
+                                        </div>
+                                    </form><!-- End Registration Form -->
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+        </section>
+    </main>
+
+    <!-- End #main -->
 
 
 
-
-  </main><!-- End #main -->
-<?php 
+    <?php 
 require_once ("footer.php");
 ?>
+
 
 </body>
 
--- navbar.php
+++ navbar.php
@@ -214,7 +214,7 @@ session_start();
         </li>
 
         <li>
-          <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+          <a class="dropdown-item d-flex align-items-center" href="users-profile.php">
             <i class="bi bi-person"></i>
             <span>My Profile</span>
           </a>
@@ -224,7 +224,7 @@ session_start();
         </li>
 
         <li>
-          <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+          <a class="dropdown-item d-flex align-items-center" href="users-profile.php">
             <i class="bi bi-gear"></i>
             <span>Account Settings</span>
           </a>
@@ -270,83 +270,21 @@ session_start();
     </a>
   </li><!-- End Dashboard Nav -->
 
-  <!-- <li class="nav-item">
+   <li class="nav-item">
     <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-      <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
+    <i class="ri-user-line"></i><span>User Management</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
     <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
       <li>
-        <a href="components-alerts.html">
-          <i class="bi bi-circle"></i><span>Alerts</span>
+        <a href="addmem.php">
+          <i class="bi bi-circle"></i><span>Add New Members</span>
         </a>
       </li>
-      <li>
-        <a href="components-accordion.html">
-          <i class="bi bi-circle"></i><span>Accordion</span>
-        </a>
-      </li>
-      <li>
-        <a href="components-badges.html">
-          <i class="bi bi-circle"></i><span>Badges</span>
-        </a>
-      </li>
-      <li>
-        <a href="components-breadcrumbs.html">
-          <i class="bi bi-circle"></i><span>Breadcrumbs</span>
-        </a>
-      </li>
-      <li>
-        <a href="components-buttons.html">
-          <i class="bi bi-circle"></i><span>Buttons</span>
-        </a>
-      </li>
-      <li>
-        <a href="components-cards.html">
-          <i class="bi bi-circle"></i><span>Cards</span>
-        </a>
-      </li>
-      <li>
-        <a href="components-carousel.html">
-          <i class="bi bi-circle"></i><span>Carousel</span>
-        </a>
-      </li>
-      <li>
-        <a href="components-list-group.html">
-          <i class="bi bi-circle"></i><span>List group</span>
-        </a>
-      </li>
-      <li>
-        <a href="components-modal.html">
-          <i class="bi bi-circle"></i><span>Modal</span>
-        </a>
-      </li>
-      <li>
-        <a href="components-tabs.html">
-          <i class="bi bi-circle"></i><span>Tabs</span>
-        </a>
-      </li>
-      <li>
-        <a href="components-pagination.html">
-          <i class="bi bi-circle"></i><span>Pagination</span>
-        </a>
-      </li>
-      <li>
-        <a href="components-progress.html">
-          <i class="bi bi-circle"></i><span>Progress</span>
-        </a>
-      </li>
-      <li>
-        <a href="components-spinners.html">
-          <i class="bi bi-circle"></i><span>Spinners</span>
-        </a>
-      </li>
-      <li>
-        <a href="components-tooltips.html">
-          <i class="bi bi-circle"></i><span>Tooltips</span>
-        </a>
-      </li>
+      
     </ul>
-  </li>End Components Nav -->
+  </li>
+  
+  <!-- End User Maneg Nav  -->
 
   <!-- <li class="nav-item">
     <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
