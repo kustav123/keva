11502ed40af88c53d05aa6f40785c5ca056e3add
Kustav <you@example.com>
2023-03-31 18:18:47 +0530

    added check for session logedin

--- navbar.php
+++ navbar.php
@@ -1,6 +1,10 @@
 <?php
 // Initialize the session
 session_start();
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+  header("location: login.php");
+  exit;
+}
 ?>
  <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
